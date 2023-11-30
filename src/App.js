e0719493c7a1504b52c01e54d1c307706314aafa
rea4203 <tna4203@naver.com>
2023-11-30 20:16:16 +0900

    2023/11/30

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import ScrollBox from './ScrollBox'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref => this.ScrollBox=ref)}/>
+        <button onClick={() => this.ScrollBox.scrollToBotton()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
